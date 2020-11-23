894b7d469ec026ddac02e60860a88bbefcf4d008
Ilya <ilya.fastovets@gmail.com>
2020-11-22 20:23:24 -0800

    study cpp references

---
+++ references.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int &r=a; // reference does not consume memory
+
+    int b=25; 
+    r=b;
+
+    cout<<a<<endl<<r<<endl;
+    return 0;
+}
