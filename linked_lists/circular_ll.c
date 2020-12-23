82c9d96196abd9292ca326fc1b62bb57ead0af57
fastovetsilya <ilya.fastovets@gmail.com>
2020-12-23 11:58:38 -0800

    insert in circular linked list

--- linked_lists/circular_ll.c
+++ linked_lists/circular_ll.c
@@ -48,10 +48,56 @@ void RDisplay(struct Node *h)
     flag=0;
 }
 
+int Length(struct Node *p)
+{
+    int len=0;
+    do
+    {
+        len++;
+        p=p->next;
+    }while(p!=Head);
+    return len;
+}
+
+void Insert(struct Node *p,int index,int x)
+{
+    struct Node *t;
+    int i;
+    if(index<0 || index>Length(p))
+        return;
+
+    if(index==0)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=x;
+        if(Head==NULL)
+        {
+            Head=t;
+            Head->next=Head;
+        }
+        else
+        {
+            while(p->next!=Head)p=p->next;
+            p->next=t;
+            t->next=Head;
+            Head=t;
+        }
+    }
+    else
+    {
+        for(i=0;i<index-1;i++)p=p->next;
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=x;
+        t->next=p->next;
+        p->next=t;
+    }
+}
+
 int main()
 {
     int A[]={2,3,4,5,6};
     create(A,5);
+    Insert(Head,2,10);
     RDisplay(Head);
 
     return 0;
