090151b3e1b748321c633594e4222c58b0e2a666
fastovetsilya <ilya.fastovets@gmail.com>
2021-01-28 16:28:46 -0800

    insertion sort

--- sorting/insertion.c
+++ sorting/insertion.c
@@ -25,11 +25,28 @@ void Bubble(int A[],int n)
     }
 }
 
+void Insertion(int A[],int n)
+{
+    int i,j,x;
+
+    for(i=1;i<n;i++)
+    {
+        j=i-1;
+        x=A[i];
+        while(j>-1 && A[j]>x)
+        {
+            A[j+1]=A[j];
+            j--;
+        }
+        A[j+1]=x;
+    }
+}
+
 int main()
 {
     int A[]={3,7,9,10,6,5,12,4,11,2},n=10,i;
 
-    Bubble(A,10);
+    Insertion(A,n);
 
     for(i=0;i<10;i++)
         printf("%d ",A[i]);
